236dc28e7ea06b309b9a2c96fec1f2ff32ee3153
ilyab <i-baykov@internet.ru>
2023-09-11 19:04:54 +0300

    Реализовал проверку соприкосновения шара с платформой

--- main.py
+++ main.py
@@ -31,6 +31,12 @@ class Platforms:
         pygame.draw.line(GameField.screen, self.color, (self.x - Platforms.size // 2, self.y),
                          (self.x + Platforms.size // 2, self.y), 4)
 
+    def check_collision(self):
+        if (self.x - Platforms.size // 2 < Ball.coordinates[0] < self.x + Platforms.size // 2 and Ball.coordinates[1] +
+                Ball.size == self.y):
+            print("КОНТАКТ!")
+            return True
+
 
 player_1 = Platforms(GameField.WIDTH // 2, 150, GREEN)
 player_2 = Platforms(GameField.WIDTH // 2, GameField.HEIGHT - 150, BLUE)
@@ -45,6 +51,9 @@ class Ball:
     color = RED
     size = 10
 
+    def get_coordinates(self):
+        return
+
     @classmethod
     def draw(cls):
         pygame.draw.circle(GameField.screen, cls.color, cls.coordinates, 10)
@@ -52,6 +61,11 @@ class Ball:
     @classmethod
     def drop(cls, side):
         cls.coordinates = cls.coordinates[0], cls.coordinates[1] + (cls.size * side)
+
+
+class Collision:
+    def check_platform(self):
+        pass
 
 
 class GameLoop:
@@ -64,10 +78,13 @@ class GameLoop:
                 break
 
         GameField.draw()
+
         Ball.draw()
         Ball.drop(1)
         player_1.draw()
         player_2.draw()
+        Platforms.check_collision(player_1)
+        Platforms.check_collision(player_2)
         time.sleep(0.03)
         pygame.display.update()
 
